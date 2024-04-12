53d29346a882efa4b0f72d48eaef99340cf2b4a8
SupLeonardo <levabaluevvv@gmail.com>
2024-04-12 22:36:54 +0500

    nepon

---
+++ bot.ts
@@ -0,0 +1,17 @@
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
+
+const bot = new Telegraf('6999026340:AAGMF_jcrLH3qULSPj41I7H16KVz1BM8pDA')
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('hipster', Telegraf.reply('λ'))
+
+
+bot.launch()
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
