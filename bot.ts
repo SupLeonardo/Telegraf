fecd033c6817ff00d0b584594346b6dbebce727b
SupLeonardo <levabaluevvv@gmail.com>
2024-04-14 20:33:17 +0500

    wow

--- bot.ts
+++ bot.ts
@@ -1,14 +1,26 @@
-const { Telegraf } = require('telegraf')
-const { message } = require('telegraf/filters')
+import { Markup, Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
 
 const bot = new Telegraf('6999026340:AAGMF_jcrLH3qULSPj41I7H16KVz1BM8pDA')
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.start((ctx) => ctx.reply("Welcome! I'm a restaurant bot. What you want to eat?"))
 
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('hipster', Telegraf.reply('λ'))   
+const inlineKeyboard = Markup.inlineKeyboard([
+    [Markup.button.callback('Button 1', 'callback1')],
+    [Markup.button.callback('Button 2', 'callback2')]
+]);
+
+bot.action('callback1', (ctx) => {
+    ctx.reply('Good day')
+})
+
+bot.action('callback2', async (ctx) => {
+    let result = await ctx.reply("Bad day");
+	bot.telegram.deleteMessage(ctx.chat.id, result.message_id-1)
+})
+
+bot.on('message', (ctx) => {
+    ctx.telegram.sendMessage(ctx.chat.id, 'Choose', inlineKeyboard)
+})
 
 
 bot.launch()
