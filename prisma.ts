f3812c659d241a5b8a0a4818f93a00591e8c1f90
SupLeonardo <levabaluevvv@gmail.com>
2024-05-07 22:37:49 +0500

    prisma

--- prisma.ts
+++ prisma.ts
@@ -1,13 +1,21 @@
-const { PrismaClient } = require('@prisma/client');
+import { PrismaClient } from '@prisma/client';
+import { Photos } from './src/photos';
+import { Descriptions } from './src/descriptions';
+import { database } from './src/keyboard';
+
 const prisma = new PrismaClient();
 
-async function createUser() {
-  try {
-    const user = await prisma.user.create({ data: { name: 'John Doe', email: 'john@example.com' } });
-    console.log(user);
-  } catch (error) {
-    console.error(error);
+async function createDish() {
+  for (let i = 0; i < database.length; i++) {
+    await prisma.dish.create({
+      data: {
+        id: i+1,
+        name: database[i],
+        photo: Photos[i],
+        description: Descriptions[i]
+      }
+    })
   }
 }
 
-createUser();
+createDish();
